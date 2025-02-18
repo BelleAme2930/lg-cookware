984dda9df4b72ce534acc782b20c065ccce7f133
Belle Ame <umair@eliyce.com>
2025-02-18 23:43:22 +0500

    Refactor

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -2,6 +2,6 @@ import Image from "@/Components/Image.jsx";
 
 export default function ApplicationLogo({className}) {
     return (
-        <Image src={'/assets/images/logo.png'} alt={'Logo'} className={'w-24'}/>
+        <Image src={'/assets/images/logo.png'} alt={'Logo'} className={`w-24 ${className}`}/>
     );
 }
