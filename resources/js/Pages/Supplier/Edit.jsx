b25b918e388430d4d16bf81ad452ed458a0cce81
Belle Ame <umair@eliyce.com>
2025-03-02 02:33:00 +0500

    Supplier Crud

--- resources/js/Pages/Supplier/Edit.jsx
+++ resources/js/Pages/Supplier/Edit.jsx
@@ -4,7 +4,6 @@ import {Head} from "@inertiajs/react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.jsx";
 import {FaArrowLeft} from "react-icons/fa";
 import LinkButton from "@/Components/LinkButton.jsx";
-import ExpenseForm from "@/Pages/Expense/Partials/ExpenseForm.jsx";
 import SupplierForm from "@/Pages/Supplier/Partials/SupplierForm.jsx";
 
 const Edit = ({supplier}) => {
