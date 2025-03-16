0688dd75e8bb610bad7408d66d73dcd1457aec44
Belle Ame <umair@eliyce.com>
2025-03-17 03:10:47 +0500

    Product Changes

--- app/Models/ProductSize.php
+++ app/Models/ProductSize.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductSize extends Model
 {
@@ -14,4 +15,9 @@ class ProductSize extends Model
         'weight',
         'quantity',
     ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
