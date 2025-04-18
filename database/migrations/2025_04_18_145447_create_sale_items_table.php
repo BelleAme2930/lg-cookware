b0c96727c9dc42a96f0b53977af7def37e7ab235
umair-eliyce <umair@eliyce.com>
2025-04-18 19:55:28 +0500

    Sale Items table

---
+++ database/migrations/2025_04_18_145447_create_sale_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sale_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sale_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_size_id')->constrained('product_sizes')->onDelete('cascade');
+            $table->unsignedBigInteger('weight')->nullable();
+            $table->unsignedBigInteger('quantity');
+            $table->unsignedBigInteger('unit_price');
+            $table->unsignedBigInteger('total_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sale_items');
+    }
+};
